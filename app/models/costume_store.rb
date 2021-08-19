960c3104e4f198d7c855c9b440b9fbff4dd96042
Hannah Malm <hannahemalm616@gmail.com>
2021-08-18 22:26:26 -0400

    Done.

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,12 @@
 # Create your CostumeStore class here
+#This model will create a Costume model, mapped to a costumes table in the database
+#Have the ability to map the columns of each row in that table with the attributes of instances
+#you can easily overrid default conventions
+
+class Costume < ActiveRecord::Base
+end 
+
+#Examples of Model/Class : Article, LineItem, Deer
+#Examples of Table/Schema : articles, line_items, deers 
+
+#Models just create the model, db/migrate creates the actual tables and migrates them to the database 
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,3 +9,19 @@
 # value types according to the spec
 
 # !!! Before you run rake db:migrate, remember to fill out the other migration files -- otherwise you'll get an error resulting from the blank migration files.
+
+
+#RUN BUNDLE INSTALL
+#CREATE THE MODEL, THEN CREATE THE MIGRATION
+class CreateCostumes < ActiveRecord::Migration[4.2]
+    def change 
+        create_table :costumes do |t|
+            t.string :name 
+            t.string :size
+            t.string :image_url
+            t.float :price
+            t.timestamp :created_at
+            t.timestamp :updated_at
+        end 
+    end 
+end 
